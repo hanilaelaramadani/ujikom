2980478979c0f7c71a63143ae35216075296d409
hanilaelaramadani <hanilaela516@gmail.com>
2024-04-26 10:13:04 +0700

    first commit

---
+++ app/Models/Instansi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instansi extends Model
+{
+    use HasFactory;
+    protected $table = 'instansi'; // Tambahkan ini
+    protected $fillable = ['nama', 'no_telp', 'instansi'];
+}
